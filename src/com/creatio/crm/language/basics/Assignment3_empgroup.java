7743739245c57d1cc590baed70d98b562493f6d6
Soumya Kulkarni <soumyak819@gmail.com>
2024-12-26 19:04:46 +0530

    Assignment2_Array

--- src/com/creatio/crm/language/basics/Assignment3_empgroup.java
+++ src/com/creatio/crm/language/basics/Assignment3_empgroup.java
@@ -3,7 +3,10 @@ package com.creatio.crm.language.basics;
 public class Assignment3_empgroup {
 
 	public static void main(String[] args) 
+		
 	{
+		
+		// two arrays created to store employee names and employee ids
 		String[] names = new String[3];
 		int[] ids = new int[3];
 		
@@ -16,6 +19,8 @@ public class Assignment3_empgroup {
 		ids[1]=obj.emp_id2;
 		ids[2]=obj.emp_id3;
 		
+		
+		//Printing employee name with ID
 		System.out.println("Employee name:" + names[0] + "," + "Employee ID:" + ids[0]);
 		System.out.println("Employee name:" + names[1] + "," + "Employee ID:" + ids[1]);
 		System.out.println("Employee name:" + names[2] + "," + "Employee ID:" + ids[2]);
