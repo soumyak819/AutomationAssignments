9a8127972bc46aa15996657ac6c5ac3dd25fe395
Soumya Kulkarni <soumyak819@gmail.com>
2025-02-12 15:58:05 +0530

    Java Assignments

---
+++ src/com/creatio/crm/language/basics/Assignment24.java
@@ -0,0 +1,30 @@
+package com.creatio.crm.language.basics;
+
+public class Assignment24 {
+	public static int maxProfit(int[] prices) {
+		if (prices == null || prices.length == 0) {
+			return 0;
+		}
+
+		int minPrice = Integer.MAX_VALUE;
+		int maxProfit = 0;
+
+		for (int price : prices) {
+			if (price < minPrice) {
+				minPrice = price;
+			} else if (price - minPrice > maxProfit) {
+				maxProfit = price - minPrice;
+			}
+		}
+
+		return maxProfit;
+	}
+
+	public static void main(String[] args) {
+		int[] prices1 = { 7, 1, 5, 3, 6, 4 };
+		System.out.println("Max Profit: " + maxProfit(prices1)); // Output: 5
+
+		int[] prices2 = { 7, 6, 4, 3, 1 };
+		System.out.println("Max Profit: " + maxProfit(prices2)); // Output: 0
+	}
+}
